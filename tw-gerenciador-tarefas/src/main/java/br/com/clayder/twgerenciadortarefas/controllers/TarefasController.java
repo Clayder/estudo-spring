bd05726a2fac3cecc363679a07d3d0057bbe247b
Peter Clayder <peterclayder@id.uff.br>
2019-07-25 10:45:35 -0300

    [tw-gerenciador-tarefas] Controller tarefas com a funcionalidade para listar as tarefas

---
+++ tw-gerenciador-tarefas/src/main/java/br/com/clayder/twgerenciadortarefas/controllers/TarefasController.java
@@ -0,0 +1,25 @@
+package br.com.clayder.twgerenciadortarefas.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.clayder.twgerenciadortarefas.repositorios.RepositorioTarefa;
+
+@Controller 
+@RequestMapping("/tarefas")
+public class TarefasController {
+
+	@Autowired
+	private RepositorioTarefa repositorioTarefa;
+
+	@GetMapping("/listar")
+	public ModelAndView listar() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("tarefas/listar");
+		mv.addObject("tarefas", repositorioTarefa.findAll());
+		return mv;
+	}
+}
